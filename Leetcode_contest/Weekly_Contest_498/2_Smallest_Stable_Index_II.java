6aa83858d802747e99738bb93f0925482ec18e75
Pragya Gupta <pragyajnv13@gmail.com>
2026-04-21 02:43:06 +0530

    Weekly contest 498, Q1 & Q2

---
+++ Leetcode_contest/Weekly_Contest_498/2_Smallest_Stable_Index_II.java
@@ -0,0 +1,68 @@
+/**
+ * Scroll below to see Java code as well
+ * 
+ * My youtube video of this question: https://youtu.be/Buo05ZkrPgs
+ * Leetcode Link: https://leetcode.com/problems/smallest-stable-index-ii/
+ */
+
+
+// C++ code
+// Approach: Precompute prefix max and maintain suffix min on the fly to check in O(1) per index whether max(left) − min(right) ≤ k.
+// TC: O(n)
+// SC: O(n)
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        vector<int> preMin(n);
+        int mn = INT_MAX;
+
+        for(int i = n-1; i>=0; i--) {
+            mn = min(mn, nums[i]);
+            preMin[i] = mn;
+        }
+
+        int mx = INT_MIN;
+
+        for(int i=0; i<n; i++) {
+            mx = max(mx, nums[i]);
+            
+            if(mx - preMin[i] <= k) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
+
+
+// Java Code
+// TC: O(n)
+// SC: O(n)
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int n = nums.length;
+
+        int[] preMin = new int[n];
+        int mn = Integer.MAX_VALUE;
+
+        for(int i = n-1; i>=0; i--) {
+            mn = Math.min(mn, nums[i]);
+            preMin[i] = mn;
+        }
+
+        int mx = Integer.MIN_VALUE;
+
+        for(int i=0; i<n; i++) {
+            mx = Math.max(mx, nums[i]);
+            
+            if(mx - preMin[i] <= k) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
