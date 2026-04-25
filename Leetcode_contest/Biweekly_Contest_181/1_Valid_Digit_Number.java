ca611d8a93e4f837fa65c6069067c4e22e330aa9
Pragya Gupta <pragyajnv13@gmail.com>
2026-04-25 22:01:58 +0530

    Qn 2 of biweekly contest 181

---
+++ Leetcode_contest/Biweekly_Contest_181/1_Valid_Digit_Number.java
@@ -0,0 +1,50 @@
+/**
+ * Scroll below to see Java code as well
+ * 
+ * My youtube video of this question: https://youtu.be/Mah3XeV2uZ8
+ * Leetcode Link: https://leetcode.com/problems/valid-digit-number/
+ */
+
+// C++ code 
+// Approach: Basic math
+// TC: O(digits)
+// SC: O(1)
+class Solution {
+public:
+    bool validDigit(int n, int x) {
+        bool found = false;
+        while(n>=10) {
+            if(n%10==x) {
+                found = true;
+            }
+            n = n/10;
+        }
+
+        if(n%10!=x && found) {
+            return true;
+        }
+
+        return false;
+    }
+};
+
+// Java Code
+// TC: O(digits)
+// SC: O(1)
+class Solution {
+    public boolean validDigit(int n, int x) {
+        boolean found = false;
+        while(n>=10) {
+            if(n%10==x) {
+                found = true;
+            }
+            n = n/10;
+        }
+
+        if(n%10!=x && found) {
+            return true;
+        }
+
+        return false;
+    }
+}
